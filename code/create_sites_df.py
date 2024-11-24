c3e308d7590826865c96e36f4400dd2d07fb68f3
rubyfest <srrubens@syr.edu>
2024-11-23 20:04:53 -0500

    made sure not to remove parts of the article that contained (strong) links. fixed the timout issue I think

--- code/create_sites_df.py
+++ code/create_sites_df.py
@@ -6,13 +6,13 @@ from playwright.sync_api import sync_playwright
 fox_urls = cache_saves.open_list('fox_urls_new')
 with sync_playwright() as playwright:
     print('Length of fox urls:', len(fox_urls))
-    #for url in fox_urls:
-    #    print(url)
-    #    print(playwright_get_site.get_fox_site(playwright, url)[0:10])
-    #    print('\n\n')
+    for url in fox_urls[2:3]:
+        print(url)
+        print(playwright_get_site.get_fox_site(playwright, url)[0:10])
+        print('\n\n')
     cbs_urls = cache_saves.open_list('cbs_urls_new')
     print('Length of cbs urls:', len(cbs_urls))
-    for url in cbs_urls:
-        print(url)
-        print(playwright_get_site.get_cbs_site(playwright, url)[0:10])
-        print('\n\n')
+    #for url in cbs_urls[0:1]:
+    #    print(url)
+    #    print(playwright_get_site.get_cbs_site(playwright, url)[0:10])
+    #    print('\n\n')
--- code/playwright_get_site.py
+++ code/playwright_get_site.py
@@ -3,9 +3,10 @@ import streamlit as st
 import pandas as pd
 
 def get_fox_site(playwright: Playwright, url: str) -> str:
-    browser = playwright.chromium.launch(headless=False, TimeoutError=200000)
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+    page.set_default_timeout(200000)
     page.goto(url)
     page.wait_for_selector('div.article-body')
     article = page.query_selector('div.article-body')
@@ -20,23 +21,39 @@ def get_fox_site(playwright: Playwright, url: str) -> str:
     print(link_text)
     for paragraph in article_text[1:-1]:
         text=paragraph.inner_text().strip()
-        print(text)
-        print(link_text[link_count])
+        #print(text)
+        #print(link_text[link_count])
         if link_text[link_count] == '\xa0' and link_count < len(link_text)-1:
             link_count+=1
             print('added')
         if link_text[link_count] in text:
-            if 'CLICK HERE TO GET THE FOX NEWS APP' in text:
-                print('done')
-            elif link_count == len(link_text)-1:
-                pass
-                print(link_count)
+            if link_text[link_count] == text:
+                if 'CLICK HERE TO GET THE FOX NEWS APP' in text:
+                    pass
+                    #print('done')
+                elif link_count == len(link_text)-1:
+                    pass
+                    #print(link_count)
+                else:
+                    #print('link count:',link_count+1,text)
+                    #print(link_count)
+                    link_count+=1
             else:
-                #print('link count:',link_count+1,text)
-                print(link_count)
-                link_count+=1
+                if 'CLICK HERE TO GET THE FOX NEWS APP' in text:
+                    text=text.replace('CLICK HERE TO GET THE FOX NEWS APP','')
+                    #print('done')
+                elif link_count == len(link_text)-1:
+                    text=text.replace(link_text[link_count],'')
+                    #print(link_count)
+                else:
+                    #print('link count:',link_count+1,text)
+                    #print(link_count)
+                    text=text.replace(link_text[link_count],'')
+                    link_count+=1
+                article.append(text)
         else:
             article.append(text)
+        print(text)
     context.close()
     browser.close()
     return ' '.join(article)
